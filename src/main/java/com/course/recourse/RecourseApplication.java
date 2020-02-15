0addf3ba7024f2080e310f0e91417f8d6a6a90ec
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-02-15 19:34:55 -0300

    Project created

---
+++ src/main/java/com/course/recourse/RecourseApplication.java
@@ -0,0 +1,13 @@
+package com.course.recourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecourseApplication.class, args);
+	}
+
+}
