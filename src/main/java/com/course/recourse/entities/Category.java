7901dd3c9984319c0f62798ba6295b70ad58030f
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-02-19 22:38:58 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/course/recourse/entities/Category.java
+++ src/main/java/com/course/recourse/entities/Category.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,8 +22,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	@Transient
+
+	// @Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 
 	public Category() {
@@ -32,7 +36,7 @@ public class Category implements Serializable {
 		this.id = id;
 		this.name = name;
 	}
-	
+
 	public Set<Product> getProducts() {
 		return products;
 	}
@@ -76,6 +80,6 @@ public class Category implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 
 }
