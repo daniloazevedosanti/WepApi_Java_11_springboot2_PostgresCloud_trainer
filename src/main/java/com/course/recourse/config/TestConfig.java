d1c3aecc82d554ee19ce72e5ef5f469a7320b3c2
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2020-02-23 17:37:19 -0300

    Payment, one-to-one association

--- src/main/java/com/course/recourse/config/TestConfig.java
+++ src/main/java/com/course/recourse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.course.recourse.entities.Category;
 import com.course.recourse.entities.Order;
 import com.course.recourse.entities.OrderItem;
+import com.course.recourse.entities.Payment;
 import com.course.recourse.entities.Product;
 import com.course.recourse.entities.User;
 import com.course.recourse.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 }
